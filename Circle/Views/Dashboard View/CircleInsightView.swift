86d589911f8bd124eff85f8eb1ebf155270f09bd
Kerby Jean <kerbyjean@Kerbys-MacBook-Pro.local>
2018-06-02 11:09:23 -0400

    changes

--- Circle/Views/Dashboard View/CircleInsightView.swift
+++ Circle/Views/Dashboard View/CircleInsightView.swift
@@ -29,7 +29,7 @@ class CircleInsightView: UIView, ListAdapterDataSource {
     lazy var adapter: ListAdapter = {
         return ListAdapter(updater: ListAdapterUpdater(), viewController: vc, workingRangeSize: 3)
     }()
-    
+     
     let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     
